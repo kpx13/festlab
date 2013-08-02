3557a319aecc1b42c07fddfcca93f5eff633de6e
Ann Kpx <annkpx@gmail.com>
2013-08-02 18:25:58 +0400

    slideshow

--- slideshow/models.py
+++ slideshow/models.py
@@ -4,6 +4,7 @@ from ckeditor.fields import RichTextField
 
 class Slider(models.Model):
     title = models.CharField(max_length=256, blank=True, verbose_name=u'заголовок')
+    image = models.ImageField(upload_to= 'uploads/slider', max_length=256, verbose_name=u'картинка')
     content = RichTextField(max_length=1024, blank=True, verbose_name=u'текст')
     sort_parameter = models.IntegerField(default=0, blank=True, verbose_name=u'порядок сортировки', help_text=u'№ слайдера: 1й, 2й .. 5й')
     
