ca97bba86bdb2a85335ccd935ea8f316adc91220
ann <ann@kpx.fvds.ru>
2013-09-03 00:27:17 +0400

    from server

---
+++ wsgi.py
@@ -0,0 +1,14 @@
+import os, sys, site
+
+root = os.path.abspath(__file__)
+
+print '***', root
+
+site_packages_root = os.path.join(root, '../lib/python2.6/site-packages')
+
+site.addsitedir(site_packages_root)
+sys.path = [os.path.dirname(root), root, site_packages_root] + sys.path
+os.environ['DJANGO_SETTINGS_MODULE'] = 'festlab.settings'
+
+import django.core.handlers.wsgi
+application = django.core.handlers.wsgi.WSGIHandler()
